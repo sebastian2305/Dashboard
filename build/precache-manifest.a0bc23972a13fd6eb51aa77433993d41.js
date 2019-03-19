e2d6d19d00c4e171ab2f504534b649ecd9784cf1
Sebastian Céspedes <sebastian.cespedes@savy.co>
2019-03-18 22:23:20 -0500

    firebase

---
+++ build/precache-manifest.a0bc23972a13fd6eb51aa77433993d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27b8692edd3eda876c5b",
+    "url": "/static/js/main.62552385.chunk.js"
+  },
+  {
+    "revision": "07ff4ecdde80d3ab2883",
+    "url": "/static/js/2.940d2349.chunk.js"
+  },
+  {
+    "revision": "27b8692edd3eda876c5b",
+    "url": "/static/css/main.dc597e1f.chunk.css"
+  },
+  {
+    "revision": "07ff4ecdde80d3ab2883",
+    "url": "/static/css/2.385072d6.chunk.css"
+  },
+  {
+    "revision": "16c28abc38f2d0b7b61c7a6425bc4559",
+    "url": "/index.html"
+  }
+];
